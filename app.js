c25fcdc1fffd32449152bee2b779be0ddf3c46d6
deepanshi-jn <deepanshijain1211@gmail.com>
2025-03-12 23:18:32 +0530

    demo

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const { GoogleGenerativeAI } = require("@google/generative-ai")
+const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors())
 
 app.post('/getResponse', async (req, res) => {
     // console.log(req.body); // Debugging
